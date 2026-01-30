efb03d3c1ec2ec86d67dcf918e02ad0f69996a95
Gilson123tenemea <tenemeaaguilar@gmail.com>
2026-01-30 05:04:03 -0500

    coneccion al back edn

--- servicios/api.js
+++ servicios/api.js
@@ -1,6 +1,6 @@
 // src/servicios/api.js
 
-export const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
+export const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
 
 if (process.env.NODE_ENV === 'development') {
   console.log('🌐 API URL:', BASE_URL);
