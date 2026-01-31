a9095d8045ad1d8a260fdc0a6bd18ad053a1c647
Gilson123tenemea <tenemeaaguilar@gmail.com>
2026-01-31 11:51:42 -0500

    solucion https

--- servicios/api.js
+++ servicios/api.js
@@ -1,3 +1,4 @@
-export const BASE_URL =
+export const BASE_URL = (
   process.env.NEXT_PUBLIC_API_URL ||
-  "https://saveworkia-backend-api-a6cpdfeuexgecka3.canadacentral-01.azurewebsites.net";
+  "https://saveworkia-backend-api-a6cpdfeuexgecka3.canadacentral-01.azurewebsites.net"
+).replace(/^http:/, "https:");
